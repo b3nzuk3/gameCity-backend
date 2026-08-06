7108f863c592cb5a32550297b6e5b1414a2ddfb9
hussein salim <hussenito7@gmail.com>
2026-08-07 02:33:21 +0300

    fix: allow local preview origins

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ app.use(
       'http://localhost:5173',
       'http://localhost:3000',
       'http://localhost:8080',
+      'http://localhost:4173',
+      'http://127.0.0.1:4173',
     ],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
